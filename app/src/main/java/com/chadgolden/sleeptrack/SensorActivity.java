ccb7070817983c58b45b1b7818aeb491e8c22254
chadgolden <chad@chadgolden.com>
2015-04-01 06:50:42 -0400

    Initial commit

---
+++ app/src/main/java/com/chadgolden/sleeptrack/SensorActivity.java
@@ -0,0 +1,123 @@
+package com.chadgolden.sleeptrack;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+public class SensorActivity extends ActionBarActivity implements SensorEventListener {
+
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+
+    private long lastUpdateTimeMillis;
+
+    private TextView moveStatus;
+    private ImageView moonImage;
+
+    private float previousX;
+    private float previousY;
+    private float previousZ;
+
+    private static final int DELAY = 1000; // In milliseconds.
+    private static final float ALLOWABLE_MOVEMENT = 0.10f;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sensor);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+
+        lastUpdateTimeMillis = System.currentTimeMillis();
+
+        moveStatus = (TextView) findViewById(R.id.textViewMoveStatus);
+        moonImage = (ImageView) findViewById(R.id.moonImage);
+
+        moonImage.bringToFront();
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_sensor, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        Sensor sensor = event.sensor;
+        long currentTimeInMillis = System.currentTimeMillis();
+
+        if (sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            if ((currentTimeInMillis - lastUpdateTimeMillis) >= DELAY) {
+                lastUpdateTimeMillis = currentTimeInMillis;
+                if (sensorExteedsAllowableMovement(event.values)) {
+                    moveStatus.setTextColor(Color.GREEN);
+                } else {
+                    moveStatus.setTextColor(Color.rgb(50,50,50));
+                }
+
+            }
+        }
+
+        previousX = event.values[0];
+        previousY = event.values[1];
+        previousZ = event.values[2];
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Not interested.
+    }
+
+    private boolean sensorExteedsAllowableMovement(float[] values) {
+        if (Math.abs(values[0] - previousX) > ALLOWABLE_MOVEMENT ||
+            Math.abs(values[1] - previousY) > ALLOWABLE_MOVEMENT ||
+            Math.abs(values[2] - previousZ) > ALLOWABLE_MOVEMENT) {
+            return true;
+        }
+        return false;
+    }
+}
